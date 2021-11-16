5460bca33423b10ce4df50e79f767a33361634e0
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-11-16 12:57:20 +0530

    Changes

--- Assignment 3/main.c
+++ Assignment 3/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
+#include "btree.h"
+#include "file_reader.h"
+
+void traverseBTree(tree_node* treeNodePtr)
+{
+    if(treeNodePtr == NULL)
+    {
+        return;
+    }
+
+    printf("Node: ");
+    for(int i=0; i<treeNodePtr->numberOfKeys; i++)
+    {
+        printf("%d ", treeNodePtr->keys[i]);
+    }
+    printf("\n");
+
+    for(int i=0; i<= treeNodePtr->numberOfKeys; i++)
+    {
+        traverseBTree(treeNodePtr->children[i]);
+    }
+
+}
 int main()
 {
+    btree* treePtr = newBtree(3);
+
+    readFile(treePtr, 1);
+
+    traverseBTree(treePtr->root);
+
+    readFile(treePtr, 0);
+
     return 0;
 }
