e0a21906f7213d400dfed88db8f7281efd7ff8a5
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2021-11-20 19:20:18 +0530

    Set up disk_simulation.h

---
+++ Assignment 3/disk_simulation.c
@@ -0,0 +1,25 @@
+#include "disk_simulation.h"
+
+void allocateNodeBTree(tree_node *treeNodePtr)
+{
+}
+
+void diskReadBTree(tree_node *treeNodePtr)
+{
+}
+
+void diskWriteBTree(tree_node *treeNodePtr)
+{
+}
+
+void allocateNodeBST(bst_node *treeNodePtr)
+{
+}
+
+void diskReadBST(bst_node *treeNodePtr)
+{
+}
+
+void diskWriteBST(bst_node *treeNodePtr)
+{
+}
